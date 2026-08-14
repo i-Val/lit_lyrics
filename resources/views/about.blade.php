75f57b53f091f4c997494169d091bdc135f5efea
i-val <ferdinantino1992@gmail.com>
2026-08-14 15:38:15 +0100

    fixed settings routing issue

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,14 +10,8 @@
         <div class="section-content" style="max-width: 800px; margin: 0 auto; text-align: left; font-size: 1.1em; line-height: 1.6;">
             <p>
                 Welcome to Lit Lyrics, your premier destination for liturgical music resources. 
-                Our mission is to simplify the preparation of music for mass and other liturgical services by providing easy access to lyrics, chords, and sheet music.
             </p>
             <br>
-            <h3>Our Mission</h3>
-            <p>
-                We strive to empower choir directors, musicians, and parishioners with tools that make selecting and organizing hymns seamless. 
-                Whether you are looking for a specific song or building a full mass lineup, Lit Lyrics is here to help.
-            </p>
             <br>
             <h3>Features</h3>
             <ul style="list-style-type: disc; margin-left: 20px;">
